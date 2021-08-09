fd92caef79b11d3e301b352bbf100a4de4db665e
User <taofiqaiyelabegan45@gmail.com>
2021-08-09 12:28:44 +0800

    added form- message

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,11 +2,11 @@ import React, {useState} from 'react'
 import './contact.scss'
 
 const Contact = () => {
-    const [message, setMessage] = useState(false)
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setMessage(true)
-    }
+    // const [message, setMessage] = useState(false)
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     setMessage(true)
+    // }
     return (
         <div className="contact" id="contact">
            <div className="left">
@@ -14,11 +14,12 @@ const Contact = () => {
            </div>
            <div className="right">
                <h2>Contact Me</h2>
-               <form onSubmit={handleSubmit}>
-                   <input type="text" placeholder="Email" />
-                   <textarea placeholder="Message"></textarea>
+               <form name="contact" method="post" data-netlify="true" onSubmit="submit">
+                   <input type="hidden" name="form-name" value="contact" />
+                   <input type="email" name="name" placeholder="Email" />
+                   <textarea name = "message" placeholder="Message"></textarea>
                    <button className="btn-grad" type="submit">Send</button>
-                   {message && <span>Thank you for reaching out. I will reply as soon as possible. </span>}
+                   {/* {message && <span>Thank you for reaching out. I will reply as soon as possible. </span>} */}
                </form>
            </div>
         </div>
